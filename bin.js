1eea9cd3831476d3edcfc12a0a632cad8372f6ac
John Hooks <bitmachina@outlook.com>
2023-01-05 13:32:39 -0800

    chore: initial commit

---
+++ bin.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+import fs from 'node:fs';
+import path from 'node:path';
+import { bold, cyan, gray, green } from 'kleur/colors';
+import prompts from 'prompts';
+import { create } from './index.js';
+
+const { version } = JSON.parse(fs.readFileSync(new URL('package.json', import.meta.url), 'utf-8'));
+
+async function main() {
+	console.log(gray(`\ncreate-pkg version ${version}`));
+
+	let cwd = process.argv[2] || '.';
+
+	if (cwd === '.') {
+		const opts = await prompts([
+			{
+				type: 'text',
+				name: 'dir',
+				message: 'Where should we create your project?\n  (leave blank to use current directory)'
+			}
+		]);
+
+		if (opts.dir) {
+			cwd = opts.dir;
+		}
+	}
+
+	if (fs.existsSync(cwd)) {
+		if (fs.readdirSync(cwd).length > 0) {
+			const response = await prompts({
+				type: 'confirm',
+				name: 'value',
+				message: 'Directory not empty. Continue?',
+				initial: false
+			});
+
+			if (!response.value) {
+				process.exit(1);
+			}
+		}
+	}
+
+	await create(cwd, { name: path.basename(path.resolve(cwd))});
+
+	console.log(bold(green('\nYour project is ready!')));
+	console.log('\nNext steps:');
+	let i = 1;
+
+	const relative = path.relative(process.cwd(), cwd);
+	if (relative !== '') {
+		console.log(`  ${i++}: ${bold(cyan(`cd ${relative}`))}`);
+	}
+
+	console.log(`  ${i++}: ${bold(cyan('npm install'))} (or pnpm install, etc)`);
+	// prettier-ignore
+	console.log(`  ${i++}: ${bold(cyan('git init && git add -A && git commit -m "Initial commit"'))} (optional)`);
+}
+
+main();
